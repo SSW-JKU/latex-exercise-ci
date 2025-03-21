3a3e7dc07effd3985a73dd7ea16be29c5857c21d
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2025-03-21 11:44:00 +0100

    Fix typo

--- run_action.sh
+++ run_action.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
 # Executes the action
-cd $GITHUB_ACTION_PATH python -m latex_build_action --config "$GITHUB_WORKSPACE/$CONFIG_FILE" --workdir "$GITHUB_WORKSPACE"
+cd $GITHUB_ACTION_PATH && python -m latex_build_action --config "$GITHUB_WORKSPACE/$CONFIG_FILE" --workdir "$GITHUB_WORKSPACE"
