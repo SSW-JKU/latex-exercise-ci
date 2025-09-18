fcea368b3e23765194c255a1ffb5b619c4f56cce
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2025-09-18 11:25:43 +0200

    Modularize integration test setup

--- action_tests/scenarios.py
+++ action_tests/scenarios.py
@@ -2,9 +2,7 @@
 Defines various integration test scenarios and their verification steps.
 """
 
-from abc import ABC, abstractmethod
-from pathlib import Path
-from typing import Any, Iterable, Optional
+from action_tests.scenario import Scenario, ScenarioManager, assert_eq, check_commit
 
 from .test_repository import DEFAULT_EMAIL, DEFAULT_USER, TestRepository, git
 
@@ -15,93 +13,11 @@ BOT_COMMIT_MSG = "Build TEX files"
 SUCCESS_OUTCOME = "success"
 FAILURE_OUTCOME = "failure"
 
-# auto-register scenario to simplify access and iteration
-_scenarios = dict[str, "Scenario"]()
 
-
-def _add_scenario(s: "Scenario") -> None:
+class BuildTestScenario(Scenario):
     """
-    Registers the given scenario in the `scenarios` dictionary.
-
-    Args:
-        s (Scenario) : The test scenario to register.
+    Abstract base class for all build action integration test scenarios.
     """
-    assert s.name not in _scenarios, f"Scenario '{s.name}' already registered."
-    _scenarios[s.name] = s
-
-
-def get_scenario(name: str) -> Optional["Scenario"]:
-    """
-    Retrieves the scenario registered under the given `name`.
-
-    Args:
-        name (str) : The name that identifies the target scenario
-
-    Returns:
-        (Scenario | None) The scenario or `None` if no such scenario is
-        registered
-    """
-    return _scenarios.get(name, None)
-
-
-def scenarios() -> Iterable[tuple[str, "Scenario"]]:
-    """
-    Returns all registered scenarios.
-
-    Returns:
-        (Iterable[tuple[str, Scenario]]) an iterable of name-scenario tuples
-    """
-    return _scenarios.items()
-
-
-def _assert(cond: bool, *msg: str) -> None:
-    """
-    Asserts that the given `cond` is `True` and otherwise raises an
-    `AssertionError` with the given `msg`.
-
-    Args:
-        cond (bool) : The condition to check
-        *msg (str) : The error message to add to the raised error
-    """
-    if not cond:
-        raise AssertionError(*msg)
-
-
-def _assert_eq(expected: Any, actual: Any, *msg: str) -> None:
-    """
-    Asserts that the given values match and raises an `AssertionError` with the
-    given `msg`.
-
-    Args:
-        expected (Any) : The expected value
-        actual (Any) : The actual value
-        *msg (str) : The error message to add to the raised error
-    """
-    _assert(expected == actual, *msg, f"Expected: <{expected}>, Actual: <{actual}>")
-
-
-def _check_commit(
-    commit: str,
-    expected_name: str,
-    expected_email: str,
-    expected_msg: Optional[str] = None,
-) -> None:
-    name, email, *msg = commit.split(":")
-    _assert_eq(expected_name, name, "Invalid commit author name")
-    _assert_eq(expected_email, email, "Invalid commit author email")
-    if expected_msg:
-        _assert_eq(expected_msg, ":".join(msg), "Mismatching commit message")
-
-
-class Scenario(ABC):
-    """
-    An abstract integration test scenario.
-    """
-
-    def __init__(self, name: str, expected_outcome: str) -> None:
-        self.name = name
-        self.path = Path(".") / "action_tests" / "_files" / name
-        self.expected_outcome = expected_outcome
 
     def get_changed_files(self, repo: TestRepository) -> list[str]:
         """
@@ -125,82 +41,6 @@ class Scenario(ABC):
         )
         return log.stdout.strip().split("\n")
 
-    def assert_files_exist(self, repo: TestRepository, *paths: list[str]) -> None:
-        """
-        Asserts that the given files exist.
-
-        Args:
-            repo (TestRepository) : The test repository that defines the
-                                    working directory
-            *paths (list[str]) : The (relative) paths to the target files
-        """
-        for path in paths:
-            self.assert_file_exists(repo, *path)
-
-    def assert_files_missing(self, repo: TestRepository, *paths: list[str]) -> None:
-        """
-        Asserts that the given files do not exist.
-
-        Args:
-            repo (TestRepository) : The test repository that defines the
-                                    working directory
-            *paths (list[str]) : The (relative) paths to the target files
-        """
-        for path in paths:
-            self.assert_file_missing(repo, *path)
-
-    def assert_file_exists(self, repo: TestRepository, *path: str) -> None:
-        """
-        Asserts that the given file exists.
-
-        Args:
-            repo (TestRepository) : The test repository that defines the
-                                    working directory
-            *path (str) : The (relative) path to the target file
-        """
-        file_path = repo.local_path.joinpath(*path)
-        _assert(
-            file_path.is_file(),
-            "File does not exist or is not a regular file:",
-            str(file_path),
-        )
-
-    def assert_file_missing(self, repo: TestRepository, *path: str) -> None:
-        """
-        Asserts that the given file does not exist.
-
-        Args:
-            repo (TestRepository) : The test repository that defines the
-                                    working directory
-            *path (str) : The (relative) path to the target file
-        """
-        file_path = repo.local_path.joinpath(*path)
-        _assert(
-            not file_path.is_file(),
-            "File does exist but shouldn't:",
-            str(file_path),
-        )
-
-    def get_oneline_log(self, repo: TestRepository) -> str:
-        """
-        Retrieves the git log output for the current repository in --oneline
-        format and using the pattern '%cn:%ce:%s'.
-
-        Args:
-            repo (TestRepository) : The test repository that defines the
-                                    working directory
-
-        Returns:
-            (str) The log output
-        """
-        return git(
-            "log",
-            "--oneline",
-            r"--format=%cn:%ce:%s",
-            check=True,
-            cwd=repo.local_path,
-        ).stdout.strip()
-
     def assert_bot_commit(
         self, repo: TestRepository, *changed_files: list[str]
     ) -> None:
@@ -212,15 +52,15 @@ class Scenario(ABC):
             repo (TestRepository) : The test repository that defines the
                                     working directory
             *changed_files (list[str]) : The list of changed files (paths
-                                         relative to the local repository)
+                                          relative to the local repository)
         """
         log = self.get_oneline_log(repo)
         lines = log.split("\n")
-        _assert_eq(2, len(lines), "Unexpected number of commits")
+        assert_eq(2, len(lines), "Unexpected number of commits")
         bot_commit, initial_commit = lines
 
-        _check_commit(initial_commit, DEFAULT_USER, DEFAULT_EMAIL)
-        _check_commit(bot_commit, BOT_NAME, BOT_EMAIL, BOT_COMMIT_MSG)
+        check_commit(initial_commit, DEFAULT_USER, DEFAULT_EMAIL)
+        check_commit(bot_commit, BOT_NAME, BOT_EMAIL, BOT_COMMIT_MSG)
 
         actual_changes = set(self.get_changed_files(repo))
 
@@ -257,35 +97,14 @@ class Scenario(ABC):
         """
         log = self.get_oneline_log(repo)
         lines = log.split("\n")
-        _assert_eq(1, len(lines), "Unexpected number of commits")
-        _check_commit(lines[0], DEFAULT_USER, DEFAULT_EMAIL)
-
-    # pylint: disable=line-too-long
-    def check_outcome(self, outcome: str) -> None:
-        """
-        Checks the previous action outcome based on the expected one.
-        (see https://docs.github.com/en/actions/reference/workflows-and-actions/contexts#steps-context).
-
-        Args:
-            outcome (str) : The outcome of the previously executed action.
-        """
-        _assert_eq(self.expected_outcome, outcome, "Invalid action outcome")
-
-    @abstractmethod
-    def verify(self, repo: TestRepository) -> None:
-        """
-        Verifies this test scenario by checking the commits and files.
-
-        Args:
-            repo (TestRepository) : The test repository that defines the
-                                    working directory
-        """
+        assert_eq(1, len(lines), "Unexpected number of commits")
+        check_commit(lines[0], DEFAULT_USER, DEFAULT_EMAIL)
 
 
 #### OLD BUILD SYSTEM ####
 
 
-class OldBuildSuccessNoChecksum(Scenario):
+class OldBuildSuccessNoChecksum(BuildTestScenario):
     """
     Integration test scenario that checks that there is always a rebuild if the
     checksum file does not exist.
@@ -311,7 +130,7 @@ class OldBuildSuccessNoChecksum(Scenario):
         self.assert_files_exist(repo, *new_files)
 
 
-class OldBuildSuccessSameChecksum(Scenario):
+class OldBuildSuccessSameChecksum(BuildTestScenario):
     """
     Integration test scenario that checks that there is no build when the
     checksum matches and all PDFs exist.
@@ -325,7 +144,7 @@ class OldBuildSuccessSameChecksum(Scenario):
         self.assert_no_bot_commit(repo)
 
 
-class OldBuildSuccessSameChecksumNoPDF(Scenario):
+class OldBuildSuccessSameChecksumNoPDF(BuildTestScenario):
     """
     Integration test scenario that checks that even a valid checksum with some
     PDFs missing does not trigger a rebuild.
@@ -348,7 +167,7 @@ class OldBuildSuccessSameChecksumNoPDF(Scenario):
         )
 
 
-class OldBuildSuccessWrongCheckSum(Scenario):
+class OldBuildSuccessWrongCheckSum(BuildTestScenario):
     """
     Integration test scenario that checks that an invalid checksum causes a
     rebuild.
@@ -374,7 +193,7 @@ class OldBuildSuccessWrongCheckSum(Scenario):
         self.assert_files_exist(repo, *modified_files)
 
 
-class OldBuildFailureNewFile(Scenario):
+class OldBuildFailureNewFile(BuildTestScenario):
     """
     Integration test scenario that checks that the checksum is not updated
     if the build fails on a new file.
@@ -404,7 +223,7 @@ class OldBuildFailureNewFile(Scenario):
         )
 
 
-class OldBuildFailureNoChecksum(Scenario):
+class OldBuildFailureNoChecksum(BuildTestScenario):
     """
     Integration test scenario that checks that there is no checksum file created
     if a build (partially) fails and no checksum file existed before.
@@ -442,7 +261,7 @@ class OldBuildFailureNoChecksum(Scenario):
         )
 
 
-class OldBuildFailureUpdateFile(Scenario):
+class OldBuildFailureUpdateFile(BuildTestScenario):
     """
     Integration test scenario that checks that the checksum file is not updated
     if a build (partially) fails due to a file update.
@@ -475,7 +294,7 @@ class OldBuildFailureUpdateFile(Scenario):
 #### NEW BUILD SYSTEM ####
 
 
-class NewBuildSuccessNoChecksum(Scenario):
+class NewBuildSuccessNoChecksum(BuildTestScenario):
     """
     Integration test scenario that checks that there is always a rebuild if the
     checksum file does not exist.
@@ -501,7 +320,7 @@ class NewBuildSuccessNoChecksum(Scenario):
         self.assert_files_exist(repo, *new_files)
 
 
-class NewBuildSuccessSameChecksum(Scenario):
+class NewBuildSuccessSameChecksum(BuildTestScenario):
     """
     Integration test scenario that checks that there is no build when the
     checksum matches and all PDFs exist.
@@ -515,7 +334,7 @@ class NewBuildSuccessSameChecksum(Scenario):
         self.assert_no_bot_commit(repo)
 
 
-class NewBuildSuccessSameChecksumNoPDF(Scenario):
+class NewBuildSuccessSameChecksumNoPDF(BuildTestScenario):
     """
     Integration test scenario that checks that even a valid checksum with some
     PDFs missing does not trigger a rebuild.
@@ -538,7 +357,7 @@ class NewBuildSuccessSameChecksumNoPDF(Scenario):
         )
 
 
-class NewBuildSuccessWrongCheckSum(Scenario):
+class NewBuildSuccessWrongCheckSum(BuildTestScenario):
     """
     Integration test scenario that checks that an invalid checksum causes a
     rebuild.
@@ -564,7 +383,7 @@ class NewBuildSuccessWrongCheckSum(Scenario):
         self.assert_files_exist(repo, *modified_files)
 
 
-class NewBuildFailureNewFile(Scenario):
+class NewBuildFailureNewFile(BuildTestScenario):
     """
     Integration test scenario that checks that the checksum is not updated
     if the build fails on a new file.
@@ -594,7 +413,7 @@ class NewBuildFailureNewFile(Scenario):
         )
 
 
-class NewBuildFailureNoChecksum(Scenario):
+class NewBuildFailureNoChecksum(BuildTestScenario):
     """
     Integration test scenario that checks that there is no checksum file created
     if a build (partially) fails and no checksum file existed before.
@@ -632,7 +451,7 @@ class NewBuildFailureNoChecksum(Scenario):
         )
 
 
-class NewBuildFailureUpdateFile(Scenario):
+class NewBuildFailureUpdateFile(BuildTestScenario):
     """
     Integration test scenario that checks that the checksum file is not updated
     if a build (partially) fails due to a file update.
@@ -662,18 +481,19 @@ class NewBuildFailureUpdateFile(Scenario):
         self.assert_files_missing(repo, *deleted_files)
 
 
-_add_scenario(OldBuildSuccessNoChecksum())
-_add_scenario(OldBuildSuccessSameChecksum())
-_add_scenario(OldBuildSuccessSameChecksumNoPDF())
-_add_scenario(OldBuildSuccessWrongCheckSum())
-_add_scenario(OldBuildFailureNewFile())
-_add_scenario(OldBuildFailureNoChecksum())
-_add_scenario(OldBuildFailureUpdateFile())
-
-_add_scenario(NewBuildSuccessNoChecksum())
-_add_scenario(NewBuildSuccessSameChecksum())
-_add_scenario(NewBuildSuccessSameChecksumNoPDF())
-_add_scenario(NewBuildSuccessWrongCheckSum())
-_add_scenario(NewBuildFailureNewFile())
-_add_scenario(NewBuildFailureNoChecksum())
-_add_scenario(NewBuildFailureUpdateFile())
+SCENARIOS = ScenarioManager(
+    OldBuildSuccessNoChecksum(),
+    OldBuildSuccessSameChecksum(),
+    OldBuildSuccessSameChecksumNoPDF(),
+    OldBuildSuccessWrongCheckSum(),
+    OldBuildFailureNewFile(),
+    OldBuildFailureNoChecksum(),
+    OldBuildFailureUpdateFile(),
+    NewBuildSuccessNoChecksum(),
+    NewBuildSuccessSameChecksum(),
+    NewBuildSuccessSameChecksumNoPDF(),
+    NewBuildSuccessWrongCheckSum(),
+    NewBuildFailureNewFile(),
+    NewBuildFailureNoChecksum(),
+    NewBuildFailureUpdateFile(),
+)
